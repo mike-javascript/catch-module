a40def5e6973d45e6ea471706d0b5058e19e3f9d
MIKE ATTAL <mike.attal26@gmail.com>
2020-06-07 15:15:46 +0300

    ok

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import * as my from "./controller.js"
+
+
+my.btns.title.addEventListener("mouseover", my.obj2.changeName);
+my.btns.title.addEventListener("click", my.obj2.conf);
+my.obj1.get();
+my.obj1.objSort();
+my.obj1.highScore();
